e5aeba4ccc3e82ac62b9ada7183deec7d940149c
rtytrf <676638449@qq.com>
2019-04-12 08:05:08 +0800

    ucos源码

---
+++ Micrium_STM32F767ZI-Nucleo_Blinky/Micrium/Software/uCOS-II/Ports/ARM-Cortex-M/ARMv7-M/IAR/os_cpu.h
@@ -0,0 +1,228 @@
+/*
+*********************************************************************************************************
+*                                                uC/OS-II
+*                                          The Real-Time Kernel
+*
+*                         (c) Copyright 1992-2018; Silicon Laboratories Inc.,
+*                                400 W. Cesar Chavez, Austin, TX 78701
+*
+*                   All rights reserved. Protected by international copyright laws.
+*
+*                  Your use of this software is subject to your acceptance of the terms
+*                  of a Silicon Labs Micrium software license, which can be obtained by
+*                  contacting info@micrium.com. If you do not agree to the terms of this
+*                  license, you may not use this software.
+*
+*                  Please help us continue to provide the Embedded community with the finest
+*                  software available. Your honesty is greatly appreciated.
+*
+*                    You can find our product's documentation at: doc.micrium.com
+*
+*                          For more information visit us at: www.micrium.com
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*
+*                                             ARMv7-M Port
+*
+* Filename  : os_cpu.h
+* Version   : V2.92.15
+*********************************************************************************************************
+* For       : ARMv7-M Cortex-M
+* Mode      : Thumb-2 ISA
+* Toolchain : IAR EWARM
+*********************************************************************************************************
+* Note(s)   : (1) This port supports the ARM Cortex-M3, Cortex-M4 and Cortex-M7 architectures.
+*             (2) It has been tested with the following Hardware Floating Point Unit.
+*                 (a) Single-precision: FPv4-SP-D16-M and FPv5-SP-D16-M
+*                 (b) Double-precision: FPv5-D16-M
+*********************************************************************************************************
+*/
+
+#ifndef  OS_CPU_H
+#define  OS_CPU_H
+
+#ifdef   OS_CPU_GLOBALS
+#define  OS_CPU_EXT
+#else
+#define  OS_CPU_EXT  extern
+#endif
+
+
+/*
+*********************************************************************************************************
+*                                     EXTERNAL C LANGUAGE LINKAGE
+*
+* Note(s) : (1) C++ compilers MUST 'extern'ally declare ALL C function prototypes & variable/object
+*               declarations for correct C language linkage.
+*********************************************************************************************************
+*/
+
+#ifdef __cplusplus
+extern  "C" {                                    /* See Note #1.                                       */
+#endif
+
+
+/*
+*********************************************************************************************************
+*                                               DEFINES
+*********************************************************************************************************
+*/
+
+#ifndef  OS_CPU_EXCEPT_STK_SIZE
+#define  OS_CPU_EXCEPT_STK_SIZE      256u        /* Default exception stack size is 256 OS_STK entries */
+#endif
+
+#ifdef __ARMVFP__
+#define  OS_CPU_ARM_FP_EN              1u
+#else
+#define  OS_CPU_ARM_FP_EN              0u
+#endif
+
+
+/*
+*********************************************************************************************************
+*                              OS TICK INTERRUPT PRIORITY CONFIGURATION
+*
+* Note(s) : (1) For systems that don't need any high, real-time priority interrupts; the tick interrupt
+*               should be configured as the highest priority interrupt but won't adversely affect system
+*               operations.
+*
+*           (2) For systems that need one or more high, real-time interrupts; these should be configured
+*               higher than the tick interrupt which MAY delay execution of the tick interrupt.
+*
+*               (a) If the higher priority interrupts do NOT continually consume CPU cycles but only
+*                   occasionally delay tick interrupts, then the real-time interrupts can successfully
+*                   handle their intermittent/periodic events with the system not losing tick interrupts
+*                   but only increasing the jitter.
+*
+*               (b) If the higher priority interrupts consume enough CPU cycles to continually delay the
+*                   tick interrupt, then the CPU/system is most likely over-burdened & can't be expected
+*                   to handle all its interrupts/tasks. The system time reference gets compromised as a
+*                   result of losing tick interrupts.
+*********************************************************************************************************
+*/
+
+#ifndef  OS_CPU_CFG_SYSTICK_PRIO
+#define  OS_CPU_CFG_SYSTICK_PRIO           0u
+#endif
+
+/*
+*********************************************************************************************************
+*                                              DATA TYPES
+*                                         (Compiler Specific)
+*********************************************************************************************************
+*/
+
+typedef unsigned char  BOOLEAN;
+typedef unsigned char  INT8U;                    /* Unsigned  8 bit quantity                           */
+typedef signed   char  INT8S;                    /* Signed    8 bit quantity                           */
+typedef unsigned short INT16U;                   /* Unsigned 16 bit quantity                           */
+typedef signed   short INT16S;                   /* Signed   16 bit quantity                           */
+typedef unsigned int   INT32U;                   /* Unsigned 32 bit quantity                           */
+typedef signed   int   INT32S;                   /* Signed   32 bit quantity                           */
+typedef float          FP32;                     /* Single precision floating point                    */
+typedef double         FP64;                     /* Double precision floating point                    */
+
+typedef unsigned int   OS_STK;                   /* Each stack entry is 32-bit wide                    */
+typedef unsigned int   OS_CPU_SR;                /* Define size of CPU status register (PSR = 32 bits) */
+
+
+/*
+*********************************************************************************************************
+*                                              Cortex-M
+*                                      Critical Section Management
+*
+* Method #1:  Disable/Enable interrupts using simple instructions.  After critical section, interrupts
+*             will be enabled even if they were disabled before entering the critical section.
+*             NOT IMPLEMENTED
+*
+* Method #2:  Disable/Enable interrupts by preserving the state of interrupts.  In other words, if
+*             interrupts were disabled before entering the critical section, they will be disabled when
+*             leaving the critical section.
+*             NOT IMPLEMENTED
+*
+* Method #3:  Disable/Enable interrupts by preserving the state of interrupts.  Generally speaking you
+*             would store the state of the interrupt disable flag in the local variable 'cpu_sr' and then
+*             disable interrupts.  'cpu_sr' is allocated in all of uC/OS-II's functions that need to
+*             disable interrupts.  You would restore the interrupt disable state by copying back 'cpu_sr'
+*             into the CPU's status register.
+*********************************************************************************************************
+*/
+
+#define  OS_CRITICAL_METHOD   3u
+
+#if OS_CRITICAL_METHOD == 3u
+#define  OS_ENTER_CRITICAL()  do { cpu_sr = OS_CPU_SR_Save();} while (0)
+#define  OS_EXIT_CRITICAL()   do { OS_CPU_SR_Restore(cpu_sr);} while (0)
+#endif
+
+
+/*
+*********************************************************************************************************
+*                                        Cortex-M Miscellaneous
+*********************************************************************************************************
+*/
+
+#define  OS_STK_GROWTH        1u                  /* Stack grows from HIGH to LOW memory on ARM        */
+
+#define  OS_TASK_SW()         OSCtxSw()
+
+
+/*
+*********************************************************************************************************
+*                                          GLOBAL VARIABLES
+*********************************************************************************************************
+*/
+
+OS_CPU_EXT  OS_STK   OS_CPU_ExceptStk[OS_CPU_EXCEPT_STK_SIZE];
+OS_CPU_EXT  OS_STK  *OS_CPU_ExceptStkBase;
+
+
+/*
+*********************************************************************************************************
+*                                         FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+#if OS_CRITICAL_METHOD == 3u                      /* See OS_CPU_A.ASM                                  */
+OS_CPU_SR  OS_CPU_SR_Save         (void);
+void       OS_CPU_SR_Restore      (OS_CPU_SR  cpu_sr);
+#endif
+
+void       OSCtxSw                (void);
+void       OSIntCtxSw             (void);
+void       OSStartHighRdy         (void);
+
+                                                  /* See OS_CPU_C.C                                    */
+void       OS_CPU_SysTickInit     (INT32U  cnts);
+void       OS_CPU_SysTickInitFreq (INT32U  cpu_freq);
+
+void       OS_CPU_SysTickHandler  (void);
+void       OS_CPU_PendSVHandler   (void);
+
+#if (OS_CPU_ARM_FP_EN > 0u)
+void       OS_CPU_FP_Reg_Push     (OS_STK    *stkPtr);
+void       OS_CPU_FP_Reg_Pop      (OS_STK    *stkPtr);
+#endif
+
+/*
+*********************************************************************************************************
+*                                   EXTERNAL C LANGUAGE LINKAGE END
+*********************************************************************************************************
+*/
+
+#ifdef __cplusplus
+}                                                 /* End of 'extern'al C lang linkage.                 */
+#endif
+
+
+/*
+*********************************************************************************************************
+*                                             MODULE END
+*********************************************************************************************************
+*/
+
+#endif
---
+++ Micrium_STM32F767ZI-Nucleo_Blinky/ReadMe.txt
@@ -0,0 +1,42 @@
+BLINKY EXAMPLE FOR THE ST STM32F767ZI Nucleo-144, REVISION B 
+
+This example project demonstrates how to create a kernel task which blinks an LED.
+
+MICRIUM PRODUCT VERSIONS
+- uC/OS-II  v2.92.15
+- uC/OS-III v3.07.03
+- uC/CPU    v1.31.04
+- uC/LIB    v1.38.02
+
+IDE/COMPILER VERSIONS
+- IAR/EWARM      v8.32.1
+- TrueSTUDIO/GNU v9.0.0
+
+HARDWARE SETUP
+- Connect USB Micro-B cable to CN1 to provide power.
+
+WORKSPACE LOCATIONS
+- ST/STM32F767ZI_Nucleo/Blinky/<OS2|OS3>/IAR/Blinky.eww
+- ST/STM32F767ZI_Nucleo/Blinky/<OS2|OS3>/TrueSTUDIO/
+
+USAGE INSTRUCTIONS
+IAR
+- Open the workspace in IAR.
+- Press F7 to build the project and Ctrl+D to start the debug session.
+- Once the debug session has started, press F5 to run the example.
+- The project creates a task which blinks an LED every 1 second.
+- Now modify the call to OSTimeDlyHMSM() in StartupTask() to increase or decrease the frequency at which the LED blinks.
+- Build and run again to see the change.
+
+TrueSTUDIO
+- Import the workspace in TrueSTUDIO and make sure the "Copy projects into workspace" Options box is UNCHECKED.
+- Press 'CTRL + B' to build the project and create a Debug session by selecting the Blinky workspace and pressing F11.
+- Once the debug session has started, press F8 to run/resume the example.
+- The project creates a task which blinks an LED every 1 second.
+- Now modify the call to OSTimeDlyHMSM() in StartupTask() to increase or decrease the frequency at which the LED blinks.
+- Build and run again to see the change.
+
+
+Please feel free to post questions or comments related to this example project at Micrium's forum page:
+
+https://www.micrium.com/forums/topic/st-stm32f767zi-nucleo-144-blinky/
